14b68eeac92e317d8a9ed2f11b60f8f3630fc94b
Benjamin Li & Karan Shetty & Kane Lee <pair+benjimon24.k-shetty.wanderfal@devbootcamp.com>
2017-09-21 16:11:44 -0400

    Add member model, member migration.

---
+++ db/migrate/20170921200117_create_members.rb
@@ -0,0 +1,20 @@
+class CreateMembers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :members do |t|
+      t.references  :district
+      t.string :state
+      t.string :first_name
+      t.string :last_name
+      t.string :facebook_account
+      t.string :twitter_account
+      t.string :youtube_account
+      t.string :party
+      t.string :next_election
+      t.string :office
+      t.string :phone
+      t.string :fax
+
+      t.timestamps
+    end
+  end
+end
