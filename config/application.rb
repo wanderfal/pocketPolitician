9dd9d1c41743463d7c5832f3e1170c9895a0e49d
benjimon24 <benji102491@gmail.com>
2017-09-22 18:10:30 -0400

    Config Rails for API

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module PocketPolitician
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.api_only = true
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
