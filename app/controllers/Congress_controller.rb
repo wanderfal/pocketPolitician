129815f5e0af3b38aeb02b4870824d64a1e1b1a3
Benjamin Li & Kane Lee <pair+benjimon24.wanderfal@devbootcamp.com>
2017-09-27 15:36:17 -0400

    Create API call for members of congress based on chamber

--- app/controllers/Congress_controller.rb
+++ app/controllers/Congress_controller.rb
@@ -1,7 +1,25 @@
 require 'net/http'
 
-CongressController < ApplicationController
+class CongressController < ApplicationController
   def index
-    uri = URI()
+    uri = URI("https://api.propublica.org/congress/v1/115/senate/members.json")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    # response = Net::HTTP.get(uri, {'X-API-Key':y3spXskaU43BBv4WCh6BazYtzVOToHf1ZUhTiiQc})
+    response = http.get(uri, {'X-API-Key':"y3spXskaU43BBv4WCh6BazYtzVOToHf1ZUhTiiQc"})
+    data = JSON.parse(response.body)
+    # ny_members = data['results'][0]['members'].select do |member|
+    #   member['state'] == "NY"
+    # end
+    ## 
+    ny_senate = data['results'][0]['members'].select do |member|
+      member['state'] == "NY"
+    end
+    # debugger
+    if ny_senate
+      render json: ny_senate
+    else
+      status 404
+    end
   end
 end
